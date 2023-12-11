ffc23e735dfa91e5b517d716e4e0000f45a31433
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-12-10 21:31:50 -0500

    Refined login interface and functionality
    - Modified login UI for improved aesthetics
    - Made enhancements to API fetching in the login process
    - Integrated signup feature for a seamless user experience
    - Implemented GetWeather function utilizing external APIs for weather information retrieval

--- MauiWeather/App.xaml.cs
+++ MauiWeather/App.xaml.cs
@@ -7,7 +7,7 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
-       MainPage = new LoginPage();
-       
-	}
+        MainPage = new NavigationPage(new LoginPage());
+
+    }
 }
